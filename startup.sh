47df8ef3eafbc034972dafd6290c26272289a513
harryyouth09 <buddyji9993@gmail.com>
2026-08-06 21:39:03 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,27 +2,9 @@
 
 echo ">> INSTALLING REQUIREMENTS..."
 cd /MusicPlayer
+
 pip3 install -U -r requirements.txt
 
 echo ">> STARTING MUSIC PLAYER USERBOT..."
-clear
-echo "
-#     #                        
-##   ## #    #  ####  #  ####  
-# # # # #    # #      # #    # 
-#  #  # #    #  ####  # #      
-#     # #    #      # # #      
-#     # #    # #    # # #    # 
-#     #  ####   ####  #  ####  
-                               
-######                                    
-#     # #        ##   #   # ###### #####  
-#     # #       #  #   # #  #      #    # 
-######  #      #    #   #   #####  #    # 
-#       #      ######   #   #      #####  
-#       #      #    #   #   #      #   #  
-#       ###### #    #   #   ###### #    # 
-                                          
-MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
-"
+
 python3 main.py
