613f8db5f3ee4ff2f3a52d3cfcc1443547d8e329
harryyouth09 <buddyji9993@gmail.com>
2026-08-06 22:04:05 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,8 +8,5 @@ cd /MusicPlayer
 python3 -m pip install --upgrade pip
 python3 -m pip install -U -r requirements.txt
 
-echo ">> TESTING SPOTIPY..."
-python3 -c "import spotipy; print('SPOTIPY OK:', spotipy.version)"
-
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 python3 main.py
